07eb0b7e88b6998651e34a52c88adecd25f830d4
drKrieger <noxnoctis35@gmail.com>
2017-02-28 23:23:08 +0100

    this is the real uncehcked commit (I learn the using of the Git)

--- JokerTheHMI.py
+++ JokerTheHMI.py
@@ -1,8 +1,26 @@
+from TransactionManagger import AccountTypes
+from AccountPrimitives import Account
 
 class JokerHMI:
-  def __init__(self):
+  accounts = []
+
+  def __init__(self, account_init_dict):
+    for dict_element in acc_init_dict:
+      acc = Account(dict_element,acc_init_dict[dict_element] )
+      self.accounts.append(acc)
+
     return
 
   def reqTransaction(self):
     return
 
+
+if __name__ == '__main__':
+  # TODO: check the modifications
+  acc_init_dict = {}
+  acc_init_dict[AccountTypes.CASH.name] = " CASH account"
+  acc_init_dict[AccountTypes.CARD.name] = "this is the CARD's account"
+
+  hmi = JokerHMI(acc_init_dict)
+
+  print "muhaha"
--- TransactionManagger.py
+++ TransactionManagger.py
@@ -1,17 +1,60 @@
-import AccountPrimitives
+from AccountPrimitives import TransactionTypes, AccountTypes
 
 class TransactionManagger():
   ## vars
   accounts = []
   ''' list of accounts; :type: list'''
-  balances = ()
+  balances = {}
   ''' collection of all current balances; :type: dict'''
 
-  ## meths
-  def doTransaction(self):
-    # get the affected accounts (which ususally will be only one account, except the withdrawal or deposit like transations)
+  def __init__(self, accounts):
+    self.accounts = accounts
+    self.updateBalances()
+    return
 
-    # request the transaction from the accounts
+  ## meths
+  def doTransaction(self, value, reason, account_name, transaction_type, time_stamp, note):
+    # get the affected accounts (which ususally will be only one account, except the withdrawal or deposit like transations)
+    # and realize the transactions on the accounts
+
+    # withdrawal
+    if transaction_type == TransactionTypes.withdrawal:
+      # get the money out of the specified account
+      for acc in self.accounts:
+        if acc.name == account_name:
+          acc.doModification(-value, reason, time_stamp, transaction_type, note)
+
+      # put the money into the CASH account
+      for acc in self.accounts:
+        if acc.name == AccountTypes.CASH.name:
+          acc.doModification(value, reason, time_stamp, transaction_type, note)
+
+    # deposit
+    if transaction_type == TransactionTypes.deposit:
+      # put the balance into the selected account
+      for acc in self.accounts:
+        if acc.name == account_name:
+          acc.doModification(value,reason, time_stamp, transaction_type, note)
+      # remove the balance from the CASH account
+      for acc in self.accounts:
+        if acc.name == AccountTypes.CASH.name:
+          acc.doModification(-value,reason,time_stamp,transaction_type,note)
+    # if the required transaction is not a deposit or withdrawal, just modify the specified account
+    if not ((transaction_type == TransactionTypes.deposit) or (transaction_type == TransactionTypes.withdrawal)):
+      for acc in self.accounts:
+        if acc.name == account_name:
+          acc.doModification(value,reason,time_stamp,transaction_type, note)
 
     # update balances
+    self.updateBalances()
     return
+
+  def updateBalances(self):
+    self.balances = {}
+    for acc_id in range(len(self.accounts)):
+      self.balances[self.accounts[acc_id].name] = self.accounts[acc_id].balance
+    return
+
+  def getBalances(self):
+    return self.balances
+
