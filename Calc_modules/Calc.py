02cd483f354949a5f97e25f463b320d485062423
asarpi <noxnoctis35@gmail.com>
2019-09-22 14:15:59 +0200

    end of big refactoring
    reseted main, removed HMI, refactored Calc

--- Calc_modules/Calc.py
+++ Calc_modules/Calc.py
@@ -1,18 +1,8 @@
-import numpy as np
-
 
 class Calc:
 
-  ## INIT
-  def __init__(self, TM):
-    self.TM = TM
-    # it's maybe nasty solution a bit, but with this way this module can access the TM module at every time without the calling of it
-    return
-
-  ## STATIC METHODS
-
   @staticmethod
-  def getChangesOfTrs(tr_list):
+  def get_all_changes_from_transactions(tr_list):
     """
     collect all balance data from the given transaction list into an array
     :param tr_list: list of transactions
@@ -24,7 +14,7 @@ class Calc:
     return changes
 
   @staticmethod
-  def getBalancesOfTrs(tr_list):
+  def get_all_balances_from_transactions(tr_list):
     """
     collect all balance data from the given transaction list into an array
     :param tr_list: list of transactions
@@ -36,7 +26,7 @@ class Calc:
     return balances
 
   @staticmethod
-  def getReasonsOfTrs(tr_list):
+  def get_all_reasons_from_transactions(tr_list):
     """
     collect all reason information from the given transaction list into an array
     :param tr_list: list of transactions
@@ -48,7 +38,7 @@ class Calc:
     return reasons
 
   @staticmethod
-  def getTimeStampsOfTrs(tr_list):
+  def get_all_time_stamps_from_trs(tr_list):
     """
     collect all time stamp data from the given transaction list into an array
     :param tr_list: list of transactions
@@ -60,7 +50,7 @@ class Calc:
     return time_stamps
 
   @staticmethod
-  def getModTypesOfTrs(tr_list):
+  def get_all_mod_types_from_transactions(tr_list):
     """
     collect all modification type information from the given transaction list into an array
     :param tr_list: list of transactions
@@ -72,7 +62,7 @@ class Calc:
     return mod_types
 
   @staticmethod
-  def getNotesOfTrs(tr_list):
+  def get_all_notest_from_transactions(tr_list):
     """
     collect all note from the given transaction list into an array
     :param tr_list: list of transactions
@@ -86,43 +76,40 @@ class Calc:
 
   ## METHODS
 
-  def selTrsInInterval(self, account_name, start_date, end_date):
+  def select_transactions_from_interval(self, account, start_date, end_date):
     """
     Method for getting all transactions between the specified time interval
+    :param transaction_manager: give the transaction manager, which encapsulates all used accounts
     :param account_name: which account is examined
     :param start_date: the first transaction's date in the result collection must be greater than this
     :param end_date: the last transaction's date in the result collection must be less or equal with this
     :return: an array of collected transactions
     """
     transactions = []
-    # get the account
-    acc = None # account
+    
+    account = None
     first_tr_id = None
     last_tr_id = None
 
-    for id in range(len(self.TM.accounts)):
-      if self.TM.accounts[id].name == account_name:
-        acc = self.TM.accounts[id]
-
     # get the first transaction ID in the specified time interval
-    for tr_id in range(1, len(acc.modification_list)):
-      if acc.modification_list[tr_id].time_stamp > start_date:
+    for tr_id in range(1, len(account.modification_list)):
+      if account.modification_list[tr_id].time_stamp > start_date:
         first_tr_id = tr_id
         break
 
     # get the last transaction ID in the specified time interval
-    if not first_tr_id == None:
-      for tr_id in range(first_tr_id, len(acc.modification_list)):
-        if (tr_id > 1) and (acc.modification_list[tr_id].time_stamp.date() > end_date.date()):
+    if first_tr_id is not None:
+      for tr_id in range(first_tr_id, len(account.modification_list)):
+        if (tr_id > 1) and (account.modification_list[tr_id].time_stamp.date() > end_date.date()):
           last_tr_id = tr_id-1
           break
-    if (last_tr_id == None) and len(acc.modification_list) > 0:
-      last_tr_id = len(acc.modification_list) -1
+    if (last_tr_id is None) and len(account.modification_list) > 0:
+      last_tr_id = len(account.modification_list) -1
 
     # iterating over all transaction between the selected two ID
     if first_tr_id < last_tr_id:
       for tr_id in range(first_tr_id, last_tr_id):
-        transactions.append(acc.modification_list[tr_id])
+        transactions.append(account.modification_list[tr_id])
     return transactions
 
 
--- TransactionManagers/TransactionManager.py
+++ TransactionManagers/TransactionManager.py
@@ -1,57 +1,63 @@
-from TransactionManagers.account import TransactionTypes, AccountTypes
-
-
-class TransactionManagger():
+#
+#from TransactionManagers.account import TransactionTypes, AccountTypes, Account
+#
+#
+#class TransactionManager():
+#
+#	def __init__(self, accounts):
+#		self.accounts = accounts
+#		''' list of accounts; :type: list'''
+#
+#		self.balances = {}
+#		''' list of current balances of all accounts; :type: dict'''
+#
+#		self.update_balances()
+#		return
+#
+#
+#	def do_transaction(self, account_name, value, reason, transaction_type, time_stamp, note):
+#		# get the affected accounts (which usually will be only one account, except the withdrawal or deposit like transactions)
+#		# and realize the transactions on the accounts
+#
+#		# withdrawal
+#		if transaction_type == TransactionTypes.withdrawal:
+#			# get the money out of the specified account
+#			for acc in self.accounts:
+#				if acc.name == account_name:
+#					acc.add_transaction(-value, reason, time_stamp, transaction_type, note)
+#
+#			# put the money into the CASH account
+#			for acc in self.accounts:
+#				if acc.name == AccountTypes.CASH.name:
+#					acc.add_transaction(value, reason, time_stamp, transaction_type, note)
+#
+#		# deposit
+#		if transaction_type == TransactionTypes.deposit:
+#			# put the balance into the selected account
+#			for acc in self.accounts:
+#				if acc.name == account_name:
+#					acc.add_transaction(value, reason, time_stamp, transaction_type, note)
+#			# remove the balance from the CASH account
+#			for acc in self.accounts:
+#				if acc.name == AccountTypes.CASH.name:
+#					acc.add_transaction(-value, reason, time_stamp, transaction_type, note)
+#
+#		# if the required transaction is not a deposit or withdrawal, just modify the specified account
+#		if not ((transaction_type == TransactionTypes.deposit) or (transaction_type == TransactionTypes.withdrawal)):
+#			for acc in self.accounts:
+#				if acc.name == account_name:
+#					acc.add_transaction(value, reason, time_stamp, transaction_type, note)
+#
+#		# update balances
+#		self.update_balances()
+#		return
+#
+#	def update_balances(self):
+#		# iterate on each accounts and get the balances of them
+#		self.balances = {}
+#		for acc_id in range(len(self.accounts)):
+#			self.balances[self.accounts[acc_id].name] = self.accounts[acc_id].balance
+#		return
+#
+#
 	
-	def __init__(self, accounts):
-		self.accounts = accounts
-		''' list of accounts; :type: list'''
-		
-		self.balances = {}
-		
-		self.update_balances()
-		return
-	
-	
-	def do_transaction(self, account_name, value, reason, transaction_type, time_stamp, note):
-		# get the affected accounts (which usually will be only one account, except the withdrawal or deposit like transactions)
-		# and realize the transactions on the accounts
-		
-		# withdrawal
-		if transaction_type == TransactionTypes.withdrawal:
-			# get the money out of the specified account
-			for acc in self.accounts:
-				if acc.name == account_name:
-					acc.add_transaction(-value, reason, time_stamp, transaction_type, note)
-			
-			# put the money into the CASH account
-			for acc in self.accounts:
-				if acc.name == AccountTypes.CASH.name:
-					acc.add_transaction(value, reason, time_stamp, transaction_type, note)
-		
-		# deposit
-		if transaction_type == TransactionTypes.deposit:
-			# put the balance into the selected account
-			for acc in self.accounts:
-				if acc.name == account_name:
-					acc.add_transaction(value, reason, time_stamp, transaction_type, note)
-			# remove the balance from the CASH account
-			for acc in self.accounts:
-				if acc.name == AccountTypes.CASH.name:
-					acc.add_transaction(-value, reason, time_stamp, transaction_type, note)
-		# if the required transaction is not a deposit or withdrawal, just modify the specified account
-		if not ((transaction_type == TransactionTypes.deposit) or (transaction_type == TransactionTypes.withdrawal)):
-			for acc in self.accounts:
-				if acc.name == account_name:
-					acc.add_transaction(value, reason, time_stamp, transaction_type, note)
-		
-		# update balances
-		self.update_balances()
-		return
-	
-	def update_balances(self):
-		# iterate on each accounts and get the balances of them
-		self.balances = {}
-		for acc_id in range(len(self.accounts)):
-			self.balances[self.accounts[acc_id].name] = self.accounts[acc_id].balance
-		return
